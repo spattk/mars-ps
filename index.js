e9625af1b8afa43680b73d7ad149c9fbd97c55e5
Sitesh Pattanaik <spattanaik@yodlee.com>
2019-06-24 20:07:28 +0530

    Fixed index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3001;
 const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
